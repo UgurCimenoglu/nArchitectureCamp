0302851119f5246fc2c00760b31b8e390285abae
Uğur ÇİMENOĞLU <ugurcimenogluu@gmail.com>
2022-08-23 03:04:47 +0300

    Add project files.

---
+++ src/demoProjects/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class BaseDbContext : DbContext
+    {
+        protected IConfiguration Configuration { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public BaseDbContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Brand>(b =>
+            {
+                b.ToTable("Brands").HasKey(k => k.Id);
+                b.Property(p => p.Id).HasColumnName("Id");
+                b.Property(p => p.Name).HasColumnName("Name");
+            });
+
+            Brand[] brands = { new(1, "Test"), new(2, "Test2"), new(3, "Test3") };
+            modelBuilder.Entity<Brand>().HasData(brands);
+        }
+    }
+}
